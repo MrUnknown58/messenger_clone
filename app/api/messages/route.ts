24d600cfd04dffd4363111f3a9a6fb55834942f1
unknown <sanusingh1603@gmail.com>
2023-09-10 14:58:55 +0530

    PusherServer in route.ts issue Fixed

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
-import { PusherServer, pusherServer } from "@/app/libs/pusher";
+import { pusherServer } from "@/app/libs/pusher";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
 export async function POST(request: Request) {
